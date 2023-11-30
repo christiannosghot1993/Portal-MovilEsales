2fc8f2ad279896672e6bc1839ceac119fa85f3ef
csuarez .ice <csuarez.ice@ibm.com>
2023-11-30 10:53:50 -0500

    Subida de codigo fuente

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portal_MovilEsales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
