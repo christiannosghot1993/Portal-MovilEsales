1805e162f2707455e95f486fedc6bccc961937c5
csuarez .ice <csuarez.ice@ibm.com>
2023-12-24 16:02:28 -0500

    Se implementa menu dashboard, carousel y happy path de clientes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Basic example
+function toogleSideBar() {
+    console.log('toogleSideBar');
+    event.preventDefault();
+    document.body.classList.toggle('sb-sidenav-toggled');
+    localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+}
+
+
+/**
+ * 
+ * Sirve para tener submenus en el menu principal
+ */
+function mostrarOcultarSubmenu(id) {
+    const element = document.getElementById(id).getElementsByTagName("ul");
+    const ancla = document.getElementById(id).getElementsByTagName("a");
+    if (element[0].className == 'aparecer-submenu') {
+        element[0].className = 'desaparecer-submenu';
+        ancla[0].className = 'ancla-principal-previous';
+    } else {
+        element[0].className = 'aparecer-submenu';
+        ancla[0].className = 'ancla-principal-next';
+    }
+
+}
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
