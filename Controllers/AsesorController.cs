cc7ae30b47011effc56c3a09eab637c1352c08bb
csuarez .ice <csuarez.ice@ibm.com>
2024-01-18 13:47:45 -0500

    Se diseña a nivel de web y responsive pantalla de estado de cuenta asesor

--- Controllers/AsesorController.cs
+++ Controllers/AsesorController.cs
@@ -11,6 +11,11 @@ namespace Portal_MovilEsales.Controllers
             return View();
         }
 
+        public IActionResult EstadoCuentaAsesor()
+        {
+            return View();
+        }
+
         public IActionResult MisClientes()
         {
             return View();
