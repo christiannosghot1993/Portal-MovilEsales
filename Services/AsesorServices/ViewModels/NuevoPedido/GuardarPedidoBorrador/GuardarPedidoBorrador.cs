75eae85c3ed00eb6ffd0a46d78731bd8b7d195d4
Juan Valencia <jvalencia@ec.kruger.com>
2024-03-01 09:58:52 -0500

    Se agrego los ViewModel´s para los servicios necesarios para agregar un nuevo pedido.

---
+++ Services/AsesorServices/ViewModels/NuevoPedido/GuardarPedidoBorrador/GuardarPedidoBorrador.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Portal_MovilEsales.Services.AsesorServices.ViewModels.NuevoPedido.GuardarPedidoBorrador
+{
+    public class GuardarPedidoBorrador
+    {
+        public string nombreCliente { get; set; }
+
+        public string formaPago { get; set; }
+
+        public string direccionEntrega { get; set; }
+
+        public string totalPeso { get; set; }
+
+        public string observacion { get; set; }
+
+        public string contacto { get; set; }
+
+        public string direccion { get; set; }
+
+        public string fechaProceso { get; set; }
+
+        public string numeroOrden { get; set; }
+
+        public string estado { get; set; }
+
+        public string numeroOrdenSAP { get; set; }
+
+        public string fechaEntrega { get; set; }
+
+        public string ordenCompra { get; set; }
+
+        public List<ListadoProductoGuardarPedidoBorrador> detallePedido { get; set; }
+
+        public string importeBruto { get; set; }
+
+        public string descuentoBase { get; set; }
+
+        public string subTotal1 { get; set; }
+
+        public string descuentoPago { get; set; }
+
+        public string descuentoRetiro { get; set; }
+
+        public string descuentoVarios { get; set; }
+
+        public string descuentoPeso { get; set; }
+
+        public string subTotal2 { get; set; }
+
+        public string iva { get; set; }
+
+        public string seguroTransporte { get; set; }
+
+        public string valorTotal { get; set; }
+
+        public string valorNcsinIva { get; set; }
+
+        public string margenPor { get; set; }
+
+    }
+}
---
+++ Services/AsesorServices/ViewModels/NuevoPedido/ProcesoFrujoAprobacion/ProcesoFlujoAprobacion.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Portal_MovilEsales.Services.AsesorServices.ViewModels.NuevoPedido.ProcesoFrujoAprobacion
+{
+    public class ProcesoFlujoAprobacion
+    {
+        public string nombreCliente { get; set; }
+
+        public string formaPago { get; set; }
+
+        public string direccionEntrega { get; set; }
+
+        public string totalPeso { get; set; }
+
+        public string observacion { get; set; }
+
+        public string contacto { get; set; }
+
+        public string direccion { get; set; }
+
+        public string fechaProceso { get; set; }
+
+        public string numeroOrden { get; set; }
+
+        public string estado { get; set; }
+
+        public string numeroOrdenSAP { get; set; }
+
+        public string fechaEntrega { get; set; }
+
+        public string ordenCompra { get; set; }
+
+        public List<ListadoProductoProcesoFlujoAprobacion> detallePedido { get; set; }
+    }
+}
