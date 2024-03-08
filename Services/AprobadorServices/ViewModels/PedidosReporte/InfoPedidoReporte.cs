000c12bc0583e8dc0839b7114251d94338dfad27
Juan Valencia <jvalencia@ec.kruger.com>
2024-03-08 17:23:38 -0500

    Se agregaron funcionalidades en las vistas del aprobador

---
+++ Services/AprobadorServices/ViewModels/PedidosReporte/InfoPedidoReporte.cs
@@ -0,0 +1,15 @@
+﻿namespace Portal_MovilEsales.Services.AprobadorServices.ViewModels.PedidosReporte
+{
+    public class InfoPedidoReporte
+    {
+        public string numeroOrdenEsales { get; set; }
+        public string numeroOrdenSAP { get; set; }
+        public string nombreCliente { get; set; }
+        public string nombreAsesor { get; set; }
+        public string margenPorcentaje { get; set; }
+        public string margenValor { get; set; }
+        public string estado { get; set; }
+        public string fechaCreacion { get; set; }
+        public string observacion { get; set; }
+    }
+}
