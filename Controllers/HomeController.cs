39c648abce808c636a187d0c6d6d88a2d421e3a1
csuarez <csuarez@NBEC-CSUAREZ>
2024-04-24 09:08:58 -0300

    Push de controller para correccion de bug al cambiar clave

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portal_MovilEsales.Models;
+using Portal_MovilEsales.Services;
 using System.Diagnostics;
 
 namespace Portal_MovilEsales.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IService _service;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IService service)
         {
-            _logger = logger;
+            _service = service;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult CambioPasswordMenu()
+        {
+            return View(); 
+        }
+
+        public IActionResult ChangePassword(string oldPass, string newPass)
+        {
+            var token = HttpContext.Session.GetString("token");
+            Authentication auth = new Authentication
+            {
+                ClaveAnterior = oldPass,
+                NuevaClave = newPass,
+                navegadorweb = "Edge"
+            };
+            string res = _service.changePassword(token, auth);
+            return Json(res);
         }
 
         public IActionResult Privacy()
